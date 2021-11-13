d99ae937a7013a61c2fc3d4d99284d4af2648952
AnyamahNdidi <theo4felix@gmail.com>
2021-11-13 23:13:50 +0100

    formilk yup reatHookForm mui all added

--- src/App.js
+++ src/App.js
@@ -5,17 +5,41 @@ import {BrowserRouter as  Router, Route, Routes} from "react-router-dom"
 import Home from "./component/Home"
 import About from "./component/About"
 import Todo from "./component/Todo"
+import {ThemeProvider} from "@mui/material/styles"
+import {theme} from "./component/ButTheme"
+import Test2 from "./component/Test2"
+import Test1 from './component/Test1';
+import {QueryClient, QueryClientProvider} from "react-query"
+import Project1 from './component/Project1';
+import Body2 from './component/Body2';
+import Body3 from './component/Body3';
+import Body from './component/Body';
+import Formyup from './component/Formyup';
 
 function App() {
+  const queryClient = new QueryClient();
   return (
+
+    <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
    <Router>
-     <Header/>
+     {/* <Header/> */}
+     <Project1/>
      <Routes>
        <Route path="" element={<Home/>}/>
        <Route path="about" element={<About/>}/>
        <Route path="Todo" element={<Todo/>}/>
+       <Route path="test2" element={<Test2/>}/>
+       <Route path="test1" element={<Test1/>}/>
+       <Route path="body" element={<Body/>}/>
+       <Route path="body2" element={<Body2/>}/>
+       <Route path="body3" element={<Body3/>}/>
+       <Route path="project1" element={<Project1/>}/>
+       <Route path="formyup" element={<Formyup/>}/>
      </Routes>
    </Router>
+   </ThemeProvider>
+   </QueryClientProvider>
   );
 }
 
--- src/component/Todo.js
+++ src/component/Todo.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import styled from "styled-components"
+import DataFile from "./DataFile"
+import Addfile from "./Addfile"
 
 function Todo() {
   return (
     <Container>
       <Wrapper>
        <Side1>
+        <DataFile/>
 
        </Side1>
        <Side2>
-
+          <Addfile/>
        </Side2>
       </Wrapper>
     </Container>
@@ -22,13 +25,16 @@ const Side1 = styled.div`
 flex: 1;
 `
 const Side2 = styled.div`
-width: 500px;
-background-color: grey;
-height: 100vh;
-min-height: 100%;
+width: 450px;
+border-left: 1px solid lightgrey;
+
 `
 
 const Container = styled.div``
 const Wrapper = styled.div`
 display: flex;
+height: 100%;
+min-height: 100vh;
+height: 100%;
+flex-wrap: wrap;
 `
